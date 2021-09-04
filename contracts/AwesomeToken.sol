47ea2f416ada744624f844a525b072250949ef6f
imsk17 <imsumitkumar17@gmail.com>
2021-09-05 00:09:13 +0530

    feat(Token): Setup Transfer Function For The Token

--- contracts/AwesomeToken.sol
+++ contracts/AwesomeToken.sol
@@ -18,9 +18,28 @@ contract AwesomeToken {
     // Maps the address to the balance of the particular address.
     mapping(address => uint256) public balanceOf;
 
+    // Event which is triggered when [from] has sent [value] tokens to [to].
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     constructor(uint256 _totalSupply) {
         // Allocate the total supply to the creator of the token.
         balanceOf[msg.sender] = _totalSupply;
         totalSupply = _totalSupply; // 1M Tokens
+    }
+
+    // Transfer _value tokens from sender's address to another.
+    // Fires the Transfer event on a successful transfer.
+    // @returns bool success - true if the transfer was successful else false.
+    // @throws if the transfer is not successful.
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        // Safely transfer the balance.
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/AwesomeToken.js
+++ test/AwesomeToken.js
@@ -44,4 +44,75 @@ contract("AwesomeToken", (accounts) => {
         );
       });
   });
+
+  it("transfers token ownership", async () => {
+    return await AwesomeToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.transfer.call(accounts[1], 999999999999999);
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "error message must contain revert"
+        );
+        return tokenInstance.transfer
+          .call(accounts[1], 250000, {
+            from: accounts[0],
+          })
+          .then((success) => {
+            assert.equal(success, true, "returns true");
+          })
+          .then(() => {
+            return tokenInstance.transfer(accounts[1], 250000, {
+              from: accounts[0],
+            });
+          });
+      })
+      .then((receipt) => {
+        assert.equal(
+          receipt.logs.length,
+          1,
+          "there must be atleast one triggered event"
+        );
+        assert.equal(
+          receipt.logs[0].event,
+          "Transfer",
+          "should be the Transfer event"
+        );
+        assert.equal(
+          receipt.logs[0].args._from,
+          accounts[0],
+          "logs the account the tokens are transferred from"
+        );
+        assert.equal(
+          receipt.logs[0].args._to,
+          accounts[1],
+          "logs the account the tokens are transferred to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          250000,
+          "logs the transfer amount"
+        );
+
+        return tokenInstance.balanceOf(accounts[1]);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          250000,
+          "adds amount to receiving account"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          750000,
+          "deducts amount from the sender's balance"
+        );
+      });
+  });
 });
