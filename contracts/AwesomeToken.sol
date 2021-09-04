d7fcce15f1f64890d8c0e75a6eb9d977d31ec6c3
imsk17 <imsumitkumar17@gmail.com>
2021-09-05 00:57:10 +0530

    feat(Token): Setup Delegated Transfers For The Token

--- contracts/AwesomeToken.sol
+++ contracts/AwesomeToken.sol
@@ -18,8 +18,21 @@ contract AwesomeToken {
     // Maps the address to the balance of the particular address.
     mapping(address => uint256) public balanceOf;
 
-    // Event which is triggered when [from] has sent [value] tokens to [to].
+    // Maps the address to the balance of the particular address.
+    // Approver => { Spender => Amount }
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // Event which is triggered when [_from] has sent [_value] tokens to [_to].
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    // Event which is triggered when a delegated transfer
+    // is approved by [_owner] of worth [_value] tokens
+    // for the spender [_spender].
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     constructor(uint256 _totalSupply) {
         // Allocate the total supply to the creator of the token.
@@ -40,6 +53,39 @@ contract AwesomeToken {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Approves [_spender] to spend [_value] tokens on behalf of the [msg.sender].
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Transfer _value tokens from [_from] address to [_yo].
+    // Fires the Transfer event on a successful transfer.
+    // @returns bool success - true if the transfer was successful else false.
+    // @throws if the transfer is not successful.
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+
+        // Transfer the balance.
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // Decrease the allowance by the amount being transfered.
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
