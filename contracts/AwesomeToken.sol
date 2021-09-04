791077911e77471f35e0c01ec502ff7872fab750
imsk17 <imsumitkumar17@gmail.com>
2021-09-04 23:04:55 +0530

    chore(solidity): Change Solidity Version to 0.8.7

--- contracts/AwesomeToken.sol
+++ contracts/AwesomeToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.7 <0.9.0;
 
 contract AwesomeToken {
     // The total amount of tokens in existence
     // Doesnt need to have a getter, because we set the visibility to public.
     uint256 public totalSupply;
 
-    constructor() public {
+    constructor() {
         totalSupply = 1000000; // 1M Tokens
     }
 }
